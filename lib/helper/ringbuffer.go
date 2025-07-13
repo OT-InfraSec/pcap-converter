abf97303a055f321c7c1f1ab0cfc16ab7bf78800
Patrick Bär <paba00014@stud.uni-saarland.de>
2025-07-13 11:55:13 +0200

    enhance ringbuffer.go and ringbuffer_test.go to support non-duplicate additions and add LIFO retrieval method

--- lib/helper/ringbuffer.go
+++ lib/helper/ringbuffer.go
@@ -5,11 +5,12 @@ import (
 )
 
 type RingBuffer[T any] struct {
-	buffer []T
-	size   int
-	mu     sync.Mutex
-	write  int
-	count  int
+	buffer    []T
+	size      int
+	mu        sync.Mutex
+	write     int
+	lastWrite int
+	count     int
 }
 
 // NewRingBuffer creates a new ring buffer with a fixed size.
@@ -26,6 +27,7 @@ func (rb *RingBuffer[T]) Add(value T) {
 	defer rb.mu.Unlock()
 
 	rb.buffer[rb.write] = value
+	rb.lastWrite = rb.write
 	rb.write = (rb.write + 1) % rb.size
 
 	if rb.count < rb.size {
@@ -33,14 +35,44 @@ func (rb *RingBuffer[T]) Add(value T) {
 	}
 }
 
+func (rb *RingBuffer[T]) AddNonDuplicate(value T, isEqual func(T, T) bool) {
+	shouldAdd := true
+	func() {
+		rb.mu.Lock()
+		defer rb.mu.Unlock()
+
+		if isEqual(value, rb.buffer[rb.lastWrite]) {
+			shouldAdd = false // Do not add if the value is a duplicate
+			return            // Do not add if the value is a duplicate
+		}
+	}()
+	if shouldAdd {
+		rb.Add(value)
+	}
+}
+
 // Get returns the contents of the buffer in FIFO order.
-func (rb *RingBuffer[T]) Get() []T {
+func (rb *RingBuffer[T]) GetAllFIFO() []T {
 	rb.mu.Lock()
 	defer rb.mu.Unlock()
 
 	result := make([]T, 0, rb.count)
 
 	for i := 0; i < rb.count; i++ {
+		index := (rb.write + rb.size - rb.count + i) % rb.size
+		result = append(result, rb.buffer[index])
+	}
+
+	return result
+}
+
+func (rb *RingBuffer[T]) GetAllLIFO() []T {
+	rb.mu.Lock()
+	defer rb.mu.Unlock()
+
+	result := make([]T, 0, rb.count)
+
+	for i := rb.count - 1; i >= 0; i-- {
 		index := (rb.write + rb.size - rb.count + i) % rb.size
 		result = append(result, rb.buffer[index])
 	}
--- lib/helper/ringbuffer_test.go
+++ lib/helper/ringbuffer_test.go
@@ -14,7 +14,7 @@ func TestRingBuffer_AddAndGet(t *testing.T) {
 	ringBuffer.Add(3)
 
 	expected := []int{1, 2, 3}
-	actual := ringBuffer.Get()
+	actual := ringBuffer.GetAllFIFO()
 	if !reflect.DeepEqual(actual, expected) {
 		t.Errorf("Expected %v, but got %v", expected, actual)
 	}
@@ -24,7 +24,7 @@ func TestRingBuffer_AddAndGet(t *testing.T) {
 	ringBuffer.Add(6)
 
 	expected = []int{2, 3, 4, 5, 6}
-	actual = ringBuffer.Get()
+	actual = ringBuffer.GetAllFIFO()
 	if !reflect.DeepEqual(actual, expected) {
 		t.Errorf("Expected %v, but got %v", expected, actual)
 	}
@@ -33,7 +33,39 @@ func TestRingBuffer_AddAndGet(t *testing.T) {
 	ringBuffer.Add(8)
 
 	expected = []int{4, 5, 6, 7, 8}
-	actual = ringBuffer.Get()
+	actual = ringBuffer.GetAllFIFO()
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Expected %v, but got %v", expected, actual)
+	}
+}
+
+func TestRingBuffer_LIFO(t *testing.T) {
+	ringBuffer := NewRingBuffer[int](5)
+	ringBuffer.Add(1)
+	ringBuffer.Add(2)
+	ringBuffer.Add(3)
+
+	expected := []int{3, 2, 1}
+	actual := ringBuffer.GetAllLIFO()
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Expected %v, but got %v", expected, actual)
+	}
+
+	ringBuffer.Add(4)
+	ringBuffer.Add(5)
+	ringBuffer.Add(6)
+
+	expected = []int{6, 5, 4, 3, 2}
+	actual = ringBuffer.GetAllLIFO()
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Expected %v, but got %v", expected, actual)
+	}
+
+	ringBuffer.Add(7)
+	ringBuffer.Add(8)
+
+	expected = []int{8, 7, 6, 5, 4}
+	actual = ringBuffer.GetAllLIFO()
 	if !reflect.DeepEqual(actual, expected) {
 		t.Errorf("Expected %v, but got %v", expected, actual)
 	}
@@ -53,7 +85,7 @@ func TestRingBufferConcurrent(t *testing.T) {
 	}
 
 	readValues := func() {
-		prices := ringBuffer.Get()
+		prices := ringBuffer.GetAllFIFO()
 		if len(prices) > 0 && len(prices) != ringBuffer.size {
 			t.Errorf("Buffer length inconsistency: expected size %d but got %d", ringBuffer.size, len(prices))
 		}
@@ -72,7 +104,154 @@ func TestRingBufferConcurrent(t *testing.T) {
 
 	wg.Wait()
 
-	finalValues := ringBuffer.Get()
+	finalValues := ringBuffer.GetAllFIFO()
+
+	for _, value := range finalValues {
+		if value < 1 || value > 8 {
+			t.Errorf("Unexpected value in buffer: %d", value)
+		}
+	}
+
+	// Ensure the buffer size is consistent with expectations
+	if len(finalValues) != ringBuffer.size {
+		t.Errorf("Expected buffer size %d, but got %d", ringBuffer.size, len(finalValues))
+	}
+}
+
+func isEqual(a, b int) bool {
+	return a == b
+}
+
+func TestRingBuffer_AddAndGet_NonDuplicate(t *testing.T) {
+	ringBuffer := NewRingBuffer[int](5)
+	ringBuffer.AddNonDuplicate(1, isEqual)
+	ringBuffer.AddNonDuplicate(2, isEqual)
+	ringBuffer.AddNonDuplicate(3, isEqual)
+
+	expected := []int{1, 2, 3}
+	actual := ringBuffer.GetAllFIFO()
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Expected %v, but got %v", expected, actual)
+	}
+
+	ringBuffer.AddNonDuplicate(4, isEqual)
+	ringBuffer.AddNonDuplicate(5, isEqual)
+	ringBuffer.AddNonDuplicate(6, isEqual)
+
+	expected = []int{2, 3, 4, 5, 6}
+	actual = ringBuffer.GetAllFIFO()
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Expected %v, but got %v", expected, actual)
+	}
+
+	ringBuffer.AddNonDuplicate(7, isEqual)
+	ringBuffer.AddNonDuplicate(8, isEqual)
+
+	expected = []int{4, 5, 6, 7, 8}
+	actual = ringBuffer.GetAllFIFO()
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Expected %v, but got %v", expected, actual)
+	}
+}
+
+func TestRingBuffer_AddAndGet_Duplicate(t *testing.T) {
+	ringBuffer := NewRingBuffer[int](5)
+	ringBuffer.AddNonDuplicate(1, isEqual)
+	ringBuffer.AddNonDuplicate(1, isEqual)
+	ringBuffer.AddNonDuplicate(3, isEqual)
+
+	expected := []int{1, 3}
+	actual := ringBuffer.GetAllFIFO()
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Expected %v, but got %v", expected, actual)
+	}
+
+	ringBuffer.AddNonDuplicate(4, isEqual)
+	ringBuffer.AddNonDuplicate(5, isEqual)
+	ringBuffer.AddNonDuplicate(5, isEqual)
+
+	expected = []int{1, 3, 4, 5}
+	actual = ringBuffer.GetAllFIFO()
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Expected %v, but got %v", expected, actual)
+	}
+
+	ringBuffer.AddNonDuplicate(7, isEqual)
+	ringBuffer.AddNonDuplicate(8, isEqual)
+
+	expected = []int{3, 4, 5, 7, 8}
+	actual = ringBuffer.GetAllFIFO()
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Expected %v, but got %v", expected, actual)
+	}
+}
+
+func TestRingBuffer_LIFO_NonDuplicate(t *testing.T) {
+	ringBuffer := NewRingBuffer[int](5)
+	ringBuffer.AddNonDuplicate(1, isEqual)
+	ringBuffer.AddNonDuplicate(2, isEqual)
+	ringBuffer.AddNonDuplicate(3, isEqual)
+
+	expected := []int{3, 2, 1}
+	actual := ringBuffer.GetAllLIFO()
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Expected %v, but got %v", expected, actual)
+	}
+
+	ringBuffer.AddNonDuplicate(4, isEqual)
+	ringBuffer.AddNonDuplicate(5, isEqual)
+	ringBuffer.AddNonDuplicate(6, isEqual)
+
+	expected = []int{6, 5, 4, 3, 2}
+	actual = ringBuffer.GetAllLIFO()
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Expected %v, but got %v", expected, actual)
+	}
+
+	ringBuffer.AddNonDuplicate(7, isEqual)
+	ringBuffer.AddNonDuplicate(8, isEqual)
+
+	expected = []int{8, 7, 6, 5, 4}
+	actual = ringBuffer.GetAllLIFO()
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Expected %v, but got %v", expected, actual)
+	}
+}
+
+func TestRingBufferConcurrent_NonDuplicate(t *testing.T) {
+	ringBuffer := NewRingBuffer[int](3)
+	var wg sync.WaitGroup
+
+	addValues := func(values []int) {
+		for _, value := range values {
+			ringBuffer.AddNonDuplicate(value, isEqual)
+			// Simulate delay
+			time.Sleep(10 * time.Millisecond)
+		}
+		wg.Done()
+	}
+
+	readValues := func() {
+		prices := ringBuffer.GetAllFIFO()
+		if len(prices) > 0 && len(prices) != ringBuffer.size {
+			t.Errorf("Buffer length inconsistency: expected size %d but got %d", ringBuffer.size, len(prices))
+		}
+		wg.Done()
+	}
+
+	wg.Add(3)
+	go addValues([]int{1, 2, 3})
+	go addValues([]int{4, 5})
+	go addValues([]int{6, 7, 8})
+
+	wg.Add(2)
+	time.Sleep(100 * time.Millisecond) // Allow some time for adds to complete
+	go readValues()
+	go readValues()
+
+	wg.Wait()
+
+	finalValues := ringBuffer.GetAllFIFO()
 
 	for _, value := range finalValues {
 		if value < 1 || value > 8 {
